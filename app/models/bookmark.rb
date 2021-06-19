4779611166628215bbdf30a5a9acdb3e32484a7f
Edmond Yeo <edmondyeois@gmail.com>
2021-06-19 15:52:15 +1000

    seed db

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: {minimum: 6}, allow_blank: false
-  validates_uniqueness_of :movie_id, scope: :list_id
-  
-
+  validates :movie, uniqueness: {scope: :list}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+buffer = open("http://tmdb.lewagon.com/movie/top_rated").read
+result = JSON.parse(buffer)
+movies = result['results']
+
+movies.each { |movie| 
+
+    title = movie['title']
+    overview = movie['overview']
+    poster_url = "https://image.tmdb.org/t/p/w500/" + movie['poster_path'].to_s
+    rating = movie["vote_average"].to_i
+    Movie.create(title: title ,overview: overview, poster_url: poster_url, rating: rating) 
+}
+
+
+
+
+
+
